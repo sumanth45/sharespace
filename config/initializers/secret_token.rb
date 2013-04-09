78ead83ac32694b16facdf3015f16eac6654ae10
sumanth45 <sumanth.nitw@gmail.com>
2013-04-09 11:25:11 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharespace::Application.config.secret_token = 'c93dddd1f9480c09e97ed915b76248181e7eec2333f33f2e3108869bf3af832969426b689fa4594e56ebae2a788b0a55e090b650a6f9abb4f7dc93cbc77ca3ac'
