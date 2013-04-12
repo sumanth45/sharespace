c2567baa0cbd464782ee07c15f99a17207f67d35
Arvind <apparao@shwetanks-lt.pramati.com>
2013-04-12 14:25:35 +0530

    Added controller method to create deals

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,6 +1,16 @@
 class DealsController < ApplicationController
 
+  def index
+    @deals = current_user.deals
+  end
+
   def new
     @deal = Deal.new
+  end
+
+  def create
+    deal = current_user.deals.build params[:deal]
+    deal.save!
+    redirect_to deals_path    
   end
 end
